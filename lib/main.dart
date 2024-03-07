9cee6cd725a8e39bd18674455d15881a6fdf2e8f
Kamal Deep Thakur <kamaldeepthakur96@gmail.com>
2024-03-07 08:14:07 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:multi_data_api/screen_with_model.dart';
+import 'package:multi_data_api/screen_without_model.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const ScreenWithOutModel(),
+    );
+  }
+}
+
